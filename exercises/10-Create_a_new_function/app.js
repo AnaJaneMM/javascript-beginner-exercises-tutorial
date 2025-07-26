6cc7a3d9cc376dc3b70e7eb255ae8aae562ba53b
AnaJaneMM <94802408+AnaJaneMM@users.noreply.github.com>
2025-07-26 19:44:27 +0000

    asd

--- exercises/10-Create_a_new_function/app.js
+++ exercises/10-Create_a_new_function/app.js
@@ -1,7 +1,9 @@
-function shortIntroduction() {
-  // Complete this function's body and arguments
+function shortIntroduction(name, profession, age) {
+  let presentation = 'Hello! my name is ' + name + ', my profession is ' + profession
+  + '. I am ' + age + ' years old.';
+  return presentation;
   
 }
 
 // Fill the gaps with your data in the correct order
-console.log(shortIntroduction(" ", " ", " "))
+console.log(shortIntroduction("Ana", "useless", "30"));
